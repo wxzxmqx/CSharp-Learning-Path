0fb380f0a950731321c1222a5686b5da5283e4f3
Serhii Yatsunenko <yatsunenko.serhii@hneu.net>
2026-01-13 09:24:01 +0100

    init an empty repository

---
+++ 01_Fundamentals/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _01_Fundamentals
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // TODO: Implement main logic here
+        }
+    }
+}
