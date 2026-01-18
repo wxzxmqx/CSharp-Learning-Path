5a225ca8dae3fc891e0cbb41191d5d4c591d84a8
Serhii Yatsunenko <yatsunenko.serhii@hneu.net>
2026-01-18 11:52:58 +0100

    fix(arc): add .idea to .gitignore to prevent the trash

--- 02_Logic/GuessTheNumber.cs
+++ 02_Logic/GuessTheNumber.cs
@@ -8,31 +8,31 @@ namespace _02_Logic
 {
     internal class GuessTheNumber
     {
-        static void Main(string[] args)
-        {
-            // Creating an instance of the random class
-            Random random = new Random();
-            int randomNumber = random.Next(1, 101);
+        //static void Main(string[] args)
+        //{
+        //    // Creating an instance of the random class
+        //    Random random = new Random();
+        //    int randomNumber = random.Next(1, 101);
 
-            Console.WriteLine("Guess the number");
-            string input = Console.ReadLine();
-            int number = 0;
+        //    Console.WriteLine("Guess the number");
+        //    string input = Console.ReadLine();
+        //    int number = 0;
 
-            bool isNumber = int.TryParse(input, out number);
+        //    bool isNumber = int.TryParse(input, out number);
 
-            if (isNumber)
-            {
-                if (number == randomNumber)
-                {
-                    Console.WriteLine("Congratulations! You've guessed the number!");
-                } else
-                {
-                    Console.WriteLine("Try again!");
-                }
-            } else
-            {
-                Console.WriteLine("Please enter the valid number.");
-            }
-        }
+        //    if (isNumber)
+        //    {
+        //        if (number == randomNumber)
+        //        {
+        //            Console.WriteLine("Congratulations! You've guessed the number!");
+        //        } else
+        //        {
+        //            Console.WriteLine("Try again!");
+        //        }
+        //    } else
+        //    {
+        //        Console.WriteLine("Please enter the valid number.");
+        //    }
+        //}
     }
 }
