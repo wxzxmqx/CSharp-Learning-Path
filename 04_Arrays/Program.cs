ff6c631c7bbaf34c94ebceff83ed896866ecaa04
wxzxmqx <yatsunenko.serhii@hneu.net>
2026-01-25 13:34:12 +0100

    feat: sum of every row of multidimensional array

--- 04_Arrays/Program.cs
+++ 04_Arrays/Program.cs
@@ -164,7 +164,24 @@
                 }
             }
 
+            Next();
+            int[,] arr =
+            {
+                { 1, 2, 3 },
+                { 4, 5, 6 },
+                { 7, 8, 9 }
+            };
 
+            for (int i = 0; i < 3; i++)
+            {
+                int sum = 0;
+                for (int j = 0; j < 3; j++)
+                {
+                    sum += arr[i, j];
+                }
+
+                Console.WriteLine(sum);
+            } 
         }
 
         static void Next()
