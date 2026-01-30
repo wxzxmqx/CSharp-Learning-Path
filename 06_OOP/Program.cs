e71e1f99b4aafb3bf1e50ee0f4691bd9e1164932
wxzxmqx <yatsunenko.serhii@hneu.net>
2026-01-30 15:44:31 +0100

    feat: implement OOP and creating instances in Main entry point

---
+++ 06_OOP/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace _06_OOP;
+
+class Program
+{
+    static void Main(string[] args) // entry point
+    {
+        // Creating an instance of the class Car
+        Car ford = new Car("A3", "Ford");
+        Car bmw = new Car("i7", "BMW");
+
+        Console.WriteLine("Enter the Brand name.");
+        
+        // set property (setter)
+        ford.Brand = "Ford";
+        
+        // get property (getter)
+        Console.WriteLine("Brand is: {0}. ", ford.Brand);
+        
+        // STUDENT DATABASE
+        Person a = new Person("Mark", "Borysov");
+        Person b = new Person("Katja", "Esenina");
+        Person c = new Person(Console.ReadLine(), Console.ReadLine());
+        
+        
+    }
+}
