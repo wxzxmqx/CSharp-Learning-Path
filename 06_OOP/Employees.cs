1c96632381fd3ff1b4a97db133ff541b8db23671
wxzxmqx <yatsunenko.serhii@hneu.net>
2026-01-31 18:35:59 +0100

    feat: implement employee system with list and methods

--- 06_OOP/Employees.cs
+++ 06_OOP/Employees.cs
@@ -2,5 +2,24 @@ namespace _06_OOP;
 
 public class Employees
 {
+    // List for storing all employees
+    public List<Employee> AllEmployees { get; set; } = new List<Employee>();
     
+    private string[] jobTitles = {
+        "Internship Student",
+        "Quality Assurance Manager",
+        "Junior Software Developer",
+        "Middle Software Developer",
+        "Senior Software Developer",
+        "HR Manager",
+        "Project Manager",
+        "CEO"
+    };
+
+    // Add a new employee to company
+    public void AddEmployee(Employee emp)
+    {
+        AllEmployees.Add(emp);
+        Console.WriteLine("{0} has been added successfully!", emp.FirstName);
+    }
 }
