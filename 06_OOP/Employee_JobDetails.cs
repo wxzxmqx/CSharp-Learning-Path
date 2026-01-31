9f5c06b5ea079badaafec004942e2333ad4eedcf
wxzxmqx <yatsunenko.serhii@hneu.net>
2026-01-31 23:20:11 +0100

    Add SubmitSickLeave, SubmitVacation, and GetWorkStatus methods
    - Checks if employee is already on sick leave or vacation
    - Prevents from taking additional sick leave or vacation
    - Updates GetWorkStatus according to the outgoing vacation/sick leave

--- 06_OOP/Employee_JobDetails.cs
+++ 06_OOP/Employee_JobDetails.cs
@@ -4,18 +4,48 @@ public partial class Employee
 {
     public string JobTitle { get; set; }
     public double Salary { get; set; }
-    public bool SickLeave { get; set; }
-    public bool Vacation { get; set; }
-    public DateTime WorkingSince { get; set; }
 
-    public void SetJobDetails(string jobTitle = "Employee", double salary = 0, bool sickLeave = false, bool vacation = false, DateTime? workingSince = null)
+    public void SetJobDetails(string jobTitle = "Employee", double salary = 0)
     {
         JobTitle = jobTitle;
         Salary = salary;
-        SickLeave = sickLeave;
-        Vacation = vacation;
-        if (workingSince == null) workingSince = DateTime.Now;
-        
-        Console.WriteLine("Job details saved successfully.");
     }
+
+    public void SubmitSickLeave(DateTime? until)
+    {
+        if (SickLeaveUntil.HasValue && DateTime.Now <= SickLeaveUntil)
+        {
+            Console.WriteLine($"The employee {FirstName} {LastName} is ALREADY on sick leave.");
+            return;
+        } else if (VacationUntil.HasValue && DateTime.Now <= VacationUntil)
+        {
+            Console.WriteLine($"The employee {FirstName} {LastName} is ALREADY on vacation. The sick leave cannot be taken during the outgoing vacation.");
+            return;
+        }
+        
+        Console.WriteLine($"The employee {FirstName} {LastName} is on sick leave.");
+        SickLeaveUntil = until;
+    }
+    public void SubmitVacation(DateTime? until)
+    {
+        if (VacationUntil.HasValue && DateTime.Now <= VacationUntil)
+        {
+            Console.WriteLine($"The employee {FirstName} {LastName} is ALREADY on vacation.");
+            return;
+        } else if (SickLeaveUntil.HasValue && DateTime.Now <= SickLeaveUntil)
+        {
+            Console.WriteLine($"The employee {FirstName} {LastName} is ALREADY on sick leave. The vacation cannot be taken during the outgoing sick leave.");
+            return;
+        }
+        
+        Console.WriteLine($"The employee {FirstName} {LastName} is on the vacation.");
+        VacationUntil = until;
+    }
+    public string GetWorkStatus()
+    {
+        if (SickLeaveUntil.HasValue && DateTime.Now <= SickLeaveUntil) return $"On sick leave until {SickLeaveUntil}";
+        if (VacationUntil.HasValue && DateTime.Now <= VacationUntil) return $"On vacation until {VacationUntil}";
+        return "Working";
+    }
+    
 }
