5ef08d2056d7bfd2ef9749ac9d8c4f95f65ec4d8
wxzxmqx <yatsunenko.serhii@hneu.net>
2026-01-31 23:31:10 +0100

    Implement DisplayAllEmployees method
    - Shows everyone who works in the company at the moment
    - Checks if employee is on vacation/sick leave

--- 06_OOP/Employees.cs
+++ 06_OOP/Employees.cs
@@ -5,7 +5,7 @@ public class Employees
     // List for storing all employees
     public List<Employee> AllEmployees { get; set; } = new List<Employee>();
     
-    private string[] jobTitles = {
+    public static string[] jobTitles = {
         "Internship Student",
         "Quality Assurance Manager",
         "Junior Software Developer",
@@ -22,4 +22,24 @@ public class Employees
         AllEmployees.Add(emp);
         Console.WriteLine("{0} has been added successfully!", emp.FirstName);
     }
+
+    public void DisplayAllEmployees()
+    {
+        int i = 0;
+            
+            foreach (var emp in AllEmployees)
+            {
+                i++;
+                if (emp.GetWorkStatus() != "Working")
+                {
+                    Console.WriteLine($"{i}. {emp.FirstName} | {emp.JobTitle} | ${emp.Salary} | {emp.GetWorkStatus()}");
+                    continue;
+                }
+                
+                Console.WriteLine($"{i}. {emp.FirstName} | {emp.JobTitle} | ${emp.Salary}");
+                
+            }
+
+    }
+
 }
