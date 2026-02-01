f0cf44af8d482c292bbedc84d222d6db78bf5a0c
wxzxmqx <yatsunenko.serhii@hneu.net>
2026-02-01 18:59:39 +0100

    add read/write-only properties (such as Password and an Id), static methods and set id for customers

--- 06_OOP/Customer.cs
+++ 06_OOP/Customer.cs
@@ -2,14 +2,28 @@ namespace _06_OOP;
 
     internal class Customer
     {
+        // Static field to hold the next ID available
+        private static int nextId = 0;
+
+        private readonly int _id; // readonly (unique identifier)
+
+        private string _password;
+        
+        // write-only property (no getter)
+        public string Password { set => _password = value; }
+        
         // Properties (w/ getters and setters)
         public string Name { get; set; } 
         public string Address { get; set; }
         public string ContactNumber { get; set; }
+        
+        // read-only property (no setter)
+        public int Id { get => _id; }
 
         // Default Constructor
         public Customer()
         {
+            _id = nextId++;
             Name = "New Customer";
             Address = "No address entered";
             ContactNumber = "No contact number entered";
@@ -19,6 +33,7 @@ namespace _06_OOP;
         // Default Parameters (N/A) if not written
         public Customer(string name, string address = "N/A", string contactNumber = "N/A")
         {
+            _id = nextId++;
             Name = name;
             Address = address;
             ContactNumber = contactNumber;
@@ -27,10 +42,21 @@ namespace _06_OOP;
         }
 
         // METHOD (sets name, address and contact number)
-        public void SetDetails(string name, string address, string contactNumber)
+        public void SetDetails(string name, string address, string contactNumber = "N/A")
         {
             Name = name;
             Address = address;
             ContactNumber = contactNumber;
+        }
+
+        public void ReadDetails()
+        {
+            Console.WriteLine($"{Name}, id: {Id}");
+        }
+        
+        // call it without creating an object
+        public static void MakeADiscount()
+        {
+            Console.WriteLine("-20% discount for ALL products has been activated.");
         }
     }
