c3d00b603d92140846232d4bcccb68ba12acbbdd
wxzxmqx <yatsunenko.serhii@hneu.net>
2026-02-01 19:02:08 +0100

    Implement Height and Width properties, read-only properties and const/readonly fields

--- 06_OOP/Rectangle.cs
+++ 06_OOP/Rectangle.cs
@@ -1,6 +1,41 @@
 namespace _06_OOP;
 
-public class Area
+internal class Rectangle
 {
-    get { return Width * Height};
+    // declaration of field (const)
+    public const int Corners = 4;
+    
+    // declaration of field (readonly) - during runtime
+    public readonly string Color;
+
+    public Rectangle(string color = "Transparent")
+    {
+        Color = color; // modifying is possible
+        // Corners = 8; // impossible due to const keyword
+    }
+
+    public void DisplayDetails()
+    {
+        Console.WriteLine($"Color: {Color}, width: {Width}, height: {Height}, corners: {Corners}");
+    }
+
+    public int SetWidth(int width)
+    {
+        Width = width;
+        return width;
+    }
+
+    public int SetHeight(int height)
+    {
+        Height = height;
+        return height;
+    }
+    
+    public double Width { get; set; }
+    public double Height { get; set; }
+
+    // Computed Property
+    
+    // Getter property (READ-ONLY PROPERTY)
+    public double Area { get => Width * Height; }
 }
